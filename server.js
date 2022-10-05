575bffba0aa19bd6d1d8cd1080171f24e4b20e6b
Kai Lew <79333361+kailew@users.noreply.github.com>
2022-10-04 18:03:30 -0700

    fix

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+
 const db = knex({
     client: 'pg',
     connection: {
@@ -19,7 +20,7 @@ app.use(cors())
 app.use(express.json());
 
 app.get('/', (req, res)=> { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin',(req,res)=>signin.handleSignin(req, res, db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
