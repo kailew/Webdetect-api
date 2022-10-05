f756e4517dce6bb273029656cce06038f09c8dec
Kai Lew <79333361+kailew@users.noreply.github.com>
2022-10-04 17:25:27 -0700

    fix

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
-        user : 'kailew',
-        password : '2253',
-        database : 'webdetect'
+        connectionString : process.env.DATABASE_URL,
+        ssl: true
     }
 }); 
 
